a0bc73a3aa08e2355044fa8d8d89c34d2e6f6e79
Kimur1 <kimur53@gmail.com>
2021-03-18 20:24:58 +0000

    index.html

--- src/js/component/Card.js
+++ src/js/component/Card.js
@@ -3,7 +3,11 @@ import React from "react";
 export const MyCard = () => {
 	return (
 		<div className="card" style={{ width: "14rem" }}>
-			<img className="card-img-top" src="..." alt="Card image here" />
+			<img
+				className="card-img-top"
+				src="https://soprema.us/wp-content/uploads/2014/12/200x200.gif"
+				alt="Card image here"
+			/>
 			<div className="card-body">
 				<h5 className="card-title">Card title</h5>
 				<p className="card-text">
